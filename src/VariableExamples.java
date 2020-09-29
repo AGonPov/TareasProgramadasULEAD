d51e6aadef19335c3e88f91e8b4902c32fc94e48
Adrian Gonzalez <adriangonzalez1501@gmail.com>
2020-09-28 18:30:32 -0600

    this is my first commit

---
+++ src/VariableExamples.java
@@ -0,0 +1,12 @@
+public class VariableExamples {
+
+    public static void main(String[] args){
+
+        short myShort = 31767;
+
+        long myLong = 2147483649L;
+
+        double myDouble = 22.55D;
+    }
+}
+
